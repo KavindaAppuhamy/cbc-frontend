0f42f7564ad55f1ca385b9b799d2dcb170d3b127
Kavinda <kavinda.appuhamy@gmail.com>
2025-05-29 15:23:49 +0530

    Hide URL using .env 🛜😊

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
     setIsLoading(true);
 
     try {
-      const response = await axios.post("http://localhost:3000/api/users/login", {
+      const response = await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login", {
         email,
         password
       });
